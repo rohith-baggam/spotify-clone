3988352b540528dec3a30a5424068cc4c1817656
rohith-baggam <rohith.baggam@aptagrim.com>
2025-07-02 22:44:39 +0530

    Auth SetUp is Done

--- lib/core/providers/current_user_notifier.g.dart
+++ lib/core/providers/current_user_notifier.g.dart
@@ -9,7 +9,16 @@ part of 'current_user_notifier.dart';
 String _$currentUserNotifierHash() =>
     r'002b81f154e99725514361878fb229b209e96ce9';
 
-/// See also [CurrentUserNotifier].
+/// This class manages the in-memory state of the currently logged-in user
+/// It is a global, reactive, app-wide provider using Riverpod
+///
+/// `@Riverpod` is a Riverpod annotation that tells Riverpod to generate
+/// boilerplate code to register and use this Notifier as a provider.
+///
+/// `keepAlive: true` ensures that the state stays alive throughout the app's lifecycle,
+/// even if no widget is currently listening to it (useful for user session)
+///
+/// Copied from [CurrentUserNotifier].
 @ProviderFor(CurrentUserNotifier)
 final currentUserNotifierProvider =
     NotifierProvider<CurrentUserNotifier, UserModel?>.internal(
